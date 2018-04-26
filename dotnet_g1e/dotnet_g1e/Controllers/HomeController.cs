8a6fe972b10336127788ec58c110cd9309c544df
Sander Fraeyman <sander.fraeyman.w6059@student.hogent.be>
2018-04-26 22:19:35 +0200

    initial doings to make the index page load sessions from the database, generates error, will continue tomorrow

--- dotnet_g1e/dotnet_g1e/Controllers/HomeController.cs
+++ dotnet_g1e/dotnet_g1e/Controllers/HomeController.cs
@@ -5,14 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dotnet_g1e.Models;
+using dotnet_g1e.Data;
+using dotnet_g1e.Data.Repositories;
 
 namespace dotnet_g1e.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SessionRepository _sessionRepository;
+        public HomeController(SessionRepository sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            
+            return View(_sessionRepository.GetAll().OrderBy(s => s.Name).ToList());
         }
 
         public IActionResult Error()
---
+++ dotnet_g1e/dotnet_g1e/Data/Repositories/SessionRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_g1e.Data.Repositories
+{
+    public class SessionRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<Session> _sessions;
+
+        public SessionRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _sessions = dbContext.Sessions;
+        }
+
+        public Session GetBy(int sessionId)
+        {
+            return _sessions.Include(s => s.Name).SingleOrDefault(s => s.SessionId == sessionId);
+        }
+
+        public IEnumerable<Session> GetAll()
+        {
+            return _sessions.Include(s => s.Name).Include(s => s.Description).ToList();
+        }
+
+        public void Add(Session session)
+        {
+            _sessions.Add(session);
+        }
+
+        public void Delete(Session session)
+        {
+            _sessions.Remove(session);
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
