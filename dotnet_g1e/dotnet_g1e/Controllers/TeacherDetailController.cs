a3afc19254bc1c7abb58574919823440858646a9
Sander Fraeyman <sander.fraeyman.w6059@student.hogent.be>
2018-04-27 22:30:45 +0200

    UC3 completed apart from Activation DR check and displaying the groups in a session

--- dotnet_g1e/dotnet_g1e/Controllers/TeacherDetailController.cs
+++ dotnet_g1e/dotnet_g1e/Controllers/TeacherDetailController.cs
@@ -2,17 +2,84 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_g1e.Models.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_g1e.Controllers
 {
-    [Authorize(Policy = "AdminOnly")]
+    //[Authorize(Policy = "AdminOnly")]
     public class TeacherDetailController : Controller
     {
-        public IActionResult Index()
+        private readonly ISessionRepository _sessionRepository;
+        public TeacherDetailController(ISessionRepository sessionRepository)
         {
+            _sessionRepository = sessionRepository;
+        }
+        public IActionResult Index(int id)
+        {
+            Session session = _sessionRepository.GetBy(id);
+            if (session == null)
+                return NotFound();
+            return View(session);
+        }
+
+        //[Authorize(Policy = "AdminOnly")]
+        public IActionResult Activate(int id)
+        {
+            Session session = _sessionRepository.GetBy(id);
+            if (session == null)
+                return NotFound();
+            ViewData[nameof(Session.Name)] = session.Name;
             return View();
+        }
+
+        //[Authorize(Policy = "AdminOnly")]
+        [HttpPost, ActionName("Activate")]
+        public IActionResult ActivateConfirmed(int id)
+        {
+            Session session = null;
+            try
+            {
+                session = _sessionRepository.GetBy(id);
+                session.ActiveSession = true;
+                _sessionRepository.SaveChanges();
+                TempData["message"] = $"You successfully activated {session.Name}.";
+            }
+            catch
+            {
+                TempData["error"] = $"Sorry, something went wrong, session {session?.Name} was not activated…";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        //[Authorize(Policy = "AdminOnly")]
+        public IActionResult Deactivate(int id)
+        {
+            Session session = _sessionRepository.GetBy(id);
+            if (session == null)
+                return NotFound();
+            ViewData[nameof(Session.Name)] = session.Name;
+            return View();
+        }
+
+        //[Authorize(Policy = "AdminOnly")]
+        [HttpPost, ActionName("Deactivate")]
+        public IActionResult DeactivateConfirmed(int id)
+        {
+            Session session = null;
+            try
+            {
+                session = _sessionRepository.GetBy(id);
+                session.ActiveSession = false;
+                _sessionRepository.SaveChanges();
+                TempData["message"] = $"You successfully deactivated {session.Name}.";
+            }
+            catch
+            {
+                TempData["error"] = $"Sorry, something went wrong, session {session?.Name} was not deactivated…";
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
